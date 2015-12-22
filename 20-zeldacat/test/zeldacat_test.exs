136fd1e0132c8cd69bcd4792f663a4a4cc62d21d
Mickey <poverwhelming@gmail.com>
2015-12-22 22:58:24 +0800

    added XYComponent and WeaponComponent to zeldacat

--- 20-zeldacat/test/zeldacat_test.exs
+++ 20-zeldacat/test/zeldacat_test.exs
@@ -17,4 +17,21 @@ defmodule ZeldacatTest do
     Entity.notify(entity, {:hit, 75})
     assert HealthComponent.alive?(entity) == false
   end
+
+  test "something with an XYComponent can move around" do
+    {:ok, entity} = Entity.init()
+    Entity.add_component(entity, XYComponent, {50, 50})
+
+    Entity.notify(entity, {:move, {:y, 35}})
+    assert XYComponent.get_position(entity) == { 50, 35}
+  end
+
+  test "something with an WeaponComponent can manage a list of weapons" do
+    {:ok, entity} = Entity.init()
+    Entity.add_component(entity, WeaponComponent, [])
+    Entity.notify(entity, {:add_weapon, "bat"})
+    assert WeaponComponent.list_weapons(entity) == ["bat"]
+    Entity.notify(entity, {:add_weapon, "screw driver"})
+    assert WeaponComponent.list_weapons(entity) == ["bat", "screw driver"]
+  end
 end
